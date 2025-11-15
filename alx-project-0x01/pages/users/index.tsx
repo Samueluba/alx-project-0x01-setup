f319323857fcf159f3675bcfca44e0ea5280dbf4
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-11-15 23:45:57 +0100

    Update index.tsx

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,1 +1,15 @@
+import Header from "@/components/layout/Header";
 
+const PostsPage: React.FC = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow p-8">
+        <h1 className="text-3xl font-bold mb-4">Posts Page</h1>
+        <p>List of posts will go here.</p>
+      </main>
+    </div>
+  )
+}
+
+export default PostsPage;
